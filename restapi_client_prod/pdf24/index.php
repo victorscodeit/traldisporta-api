f04dd0a54fcef6553e4cd58c2cbe6c5995da6de0
Victor Sancho <victor.sancho.coma@gmail.com>
2026-03-23 15:28:10 +0100

    Fix formatting in index.php by adding a trailing space after CURLOPT_MAXREDIRS for consistency.

--- restapi_client_prod/pdf24/index.php
+++ restapi_client_prod/pdf24/index.php
@@ -6,7 +6,7 @@ curl_setopt_array($curl, array(
   CURLOPT_URL => 'http://91.187.69.73:8080/restapi_prod/v1/generateExpeditionPdfs',
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_MAXREDIRS => 10, 
   CURLOPT_TIMEOUT => 0,
   CURLOPT_FOLLOWLOCATION => true,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
